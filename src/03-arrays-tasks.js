95773c7d0fb20227452c46708e92f1324fcd3bdd
Alexander Bazhenov <alexbazhen@yandex.ru>
2022-07-24 02:40:29 +0300

    feat: add solution for distinct in 03-arrays-tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -488,8 +488,14 @@ function getIntervalArray(start, end) {
  *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  const arrCheck = [];
+  arr.map((val) => {
+    if (arrCheck.indexOf(val) >= 0) return null;
+    arrCheck.push(val);
+    return val;
+  });
+  return arrCheck;
 }
 
 /**
